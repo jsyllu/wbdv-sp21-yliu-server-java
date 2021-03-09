cd06f08922f4418b2ea52da5ff56236c61cee97e
jsyllu <jessicaly.beau@foxmail.com>
2021-03-09 12:35:59 -0500

    add course data model and service

---
+++ src/main/java/com/example/econstudio/models/Course.java
@@ -0,0 +1,24 @@
+package com.example.econstudio.models;
+
+import java.util.Date;
+
+public class Course {
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
